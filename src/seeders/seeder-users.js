079a311a1ea0375146d9c777fc056b0ea6150771
choiquychoi <congquynguyen1349@gmail.com>
2024-08-07 11:35:01 +0700

    Tạo Tất Cả Models, Migrations và Seeders

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -1,13 +1,17 @@
 'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        email: 'admin@gmail.com',
+        password: "123456",
+        firstName: 'Nguyen Cong',
+        lastName: 'Quy',
+        address: 'VietNam',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole:'R1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
